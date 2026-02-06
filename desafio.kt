ab035214d7d66977bfa724d9bbb8781f911f15c4
Gislaine Lilo <61096619+gililo@users.noreply.github.com>
2026-02-05 22:09:57 -0500

    Exclusao das classes de dados ConteudoEducacional e Formacao que vieram como default no desafio

--- desafio.kt
+++ desafio.kt
@@ -11,17 +11,6 @@ data class Aluno(val id: String, val nome: String)
 // Representa a Formacao (nome, nivel e lista de conteudos, alem de suas funcoes)
 class Formacao(val nome: String, val nivel: Nivel, val conteudos: MutableSet<ConteudoEducacional>)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
-}
-
 fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
